28a1d94f64a47634e63d01a08dfe5d641e3806c3
sossa577 <laurasossa577@gmail.com>
2026-09-19 16:14:16 -0500

    Add match detail page and board filters (Module 5)

--- GolBet.Services/Mapping/MappingProfile.cs
+++ GolBet.Services/Mapping/MappingProfile.cs
@@ -9,5 +9,10 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<Match, MatchDto>();
+
+        CreateMap<Match, MatchDetailDto>()
+            .ForMember(dto => dto.TotalBets, //Destino
+               options => options.MapFrom(match => /*Mapeo*/
+                    match.Bets.Count)); //Origen
     }
 }
