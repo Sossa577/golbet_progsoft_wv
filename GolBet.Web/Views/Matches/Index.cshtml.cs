c303917e6b6ece81035f5b925f2090be02168646
sossa577 <laurasossa577@gmail.com>
2026-09-19 13:19:19 -0500

    Add services layer, DTOs, AutoMapper and match board (Module 4)

---
+++ GolBet.Web/Views/Matches/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GolBet.web.Views.Matches
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
